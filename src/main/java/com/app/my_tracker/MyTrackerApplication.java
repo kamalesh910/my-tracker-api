f276afe4c7ec49ddfe62231898b7e29bb66fe79a
kamalesh <kdhage910@gmail.com>
2024-11-24 00:57:02 +0530

    missing ; added

--- src/main/java/com/app/my_tracker/MyTrackerApplication.java
+++ src/main/java/com/app/my_tracker/MyTrackerApplication.java
@@ -8,7 +8,7 @@ public class MyTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTrackerApplication.class, args);
-		System.out.println("Application started up and running")
+		System.out.println("Application started up and running");
 	}
 
 }
