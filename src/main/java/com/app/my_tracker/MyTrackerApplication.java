63388e092a0afb01ccc02d4e72a37c6f365ebe71
kamalesh <kdhage910@gmail.com>
2024-11-26 23:02:25 +0530

    EnabledMongoRepository

--- src/main/java/com/app/my_tracker/MyTrackerApplication.java
+++ src/main/java/com/app/my_tracker/MyTrackerApplication.java
@@ -2,8 +2,10 @@ package com.app.my_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.app.my_tracker.repository")
 public class MyTrackerApplication {
 
 	public static void main(String[] args) {
